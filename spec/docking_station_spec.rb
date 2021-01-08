b3ef70528d99b955ebaa23e47c1b1391cda5b126
Reeshul <reeshul@hotmail.com>
2021-01-08 17:35:31 +0000

    added test for customised capacity

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -19,7 +19,7 @@ describe DockingStation do
     expect {subject.release_bike}.to raise_error{"Can still release a bike even though none is docked."}
   end
 
-  it "should not dock a bike if no space is available" do
+  it "should not dock a bike if no space is available at default capacity" do
     expect { 21.times { subject.dock(Bike.new) } }.to raise_error{"No space available in docking station to dock bike."}
   end
 
@@ -36,11 +36,11 @@ describe DockingStation do
     expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
   end
 
-=begin
+
   it "should, if a capacity is specified, have the specified capacity" do
     station = DockingStation.new(10)
-    expect(station.capacity).to eq 10
+    10.times { station.dock(Bike.new) }
+    expect { station.dock(Bike.new) }.to raise_error("Docking station is full.")
   end
-=end
 
 end
