d15fcb5ecb7e9f6c5228ed6e8ca8fd815cf5cec2
Joshua Sinyor <josh.sinyor@gmail.com>
2021-01-08 16:01:33 +0000

    Updated docking_station.rb and docking_station_spec.rb. This reflects Step 13.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,26 +2,20 @@ require_relative "bike"
 
 class DockingStation
 
+  attr_reader :bike
 
   def initialize
     @bike = nil
   end
 
   def release_bike
-    "Bike released."
-    raise Exception.new
+    raise Exception.new("No bike in station to release.") if @bike.nil?
+    return @bike
   end
 
   def dock(bike)
-    if @bike.nil?
-      @bike = bike
-      puts "Bike docked."
-    else
-      puts "No space available in dock."
-      raise Exception.new
-    end
+    raise Exception.new("Docking station is full.") unless @bike.nil?
+    @bike = bike
   end
-
-  attr_reader :bike
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,28 +2,40 @@ require 'docking_station'
 
 describe DockingStation do
 
-  # it "should not release a bike if no bike is available" do
-  # expect {subject.release_bike}.to raise_exception
-  # puts "No bikes docked."
-  # end
+  it "should dock a bike" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it "should not release a bike if no bike has ever been docked" do
+    expect {subject.release_bike}.to raise_error{"No bike has ever been docked, so none can be released."}
+  end
+
+=begin
+  it "should not release a bike if a bike has been docked but then released" do
+    bike = Bike.new
+    subject.dock(bike)
+    subject.release_bike
+
+    expect {subject.release_bike}.to raise_error{"Can still release a bike even though none is docked."}
+  end
+=end
 
   it "should not dock a bike if no space is available" do
-  bike = Bike.new
-  subject.dock(bike)
+    bike = Bike.new
+    subject.dock(bike)
 
-  bike = Bike.new
-  expect { subject.dock(bike) }.to raise_exception
+    bike = Bike.new
+    expect { subject.dock(bike) }.to raise_error{"No space available in docking station to dock bike."}
   end
 
 
-  # it "should release a working bike" do
-  #   bike = subject.release_bike
-  #   expect(bike.working?).to eq true
-  # end
+  it "should release a working bike" do
+    bike = Bike.new
+    subject.dock(bike)
 
-  #it "should dock a bike" do
-  #  bike = Bike.new
-  #  expect(subject.dock(bike)).to eq bike
-  #end
+    bike = subject.release_bike
+    expect(bike.working?).to eq true
+  end
 
 end
