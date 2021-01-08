4e4e9ca04c1c31c7b9a19fe93a144c78e59d9e0b
Joshua Sinyor <josh.sinyor@gmail.com>
2021-01-08 16:53:57 +0000

    Updated docking_station.rb. This reflects Step 15.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,13 +9,23 @@ class DockingStation
   end
 
   def release_bike
-    raise Exception.new("No bike in station to release.") if @bikes_docked.empty?
+    raise Exception.new("No bike in station to release.") if empty?
     @bikes_docked.pop
   end
 
   def dock(bike)
-    raise Exception.new("Docking station is full.") if @bikes_docked.count > 19
+    raise Exception.new("Docking station is full.") if full?
     @bikes_docked << bike
+  end
+
+  private
+
+  def empty?
+    @bikes_docked.count <= 0
+  end
+
+  def full?
+    @bikes_docked.count >= 20
   end
 
 end
