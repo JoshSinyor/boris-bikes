319e0c5d4e21f64f1c9d0a735771e222865c4370
Joshua Sinyor <josh.sinyor@gmail.com>
2021-01-08 17:40:41 +0000

    Updated docking_station.rb. This reflects more specific error messages.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,21 +8,20 @@ describe DockingStation do
   end
 
   it "should not release a bike if no bike has ever been docked" do
-    expect {subject.release_bike}.to raise_error{"No bike has ever been docked, so none can be released."}
+    expect {subject.release_bike}.to raise_error{"Docking station can release a bike even though none have ever been docked."}
   end
 
-  it "should not release a bike if a bike has been docked but then released" do
+  it "should not release a bike if all bikes docked have since been released" do
     bike = Bike.new
     subject.dock(bike)
     subject.release_bike
 
-    expect {subject.release_bike}.to raise_error{"Can still release a bike even though none is docked."}
+    expect {subject.release_bike}.to raise_error{"Docking station can release a bike even though all those docked have been released."}
   end
 
   it "should not dock a bike if no space is available at default capacity" do
-    expect { 21.times { subject.dock(Bike.new) } }.to raise_error{"No space available in docking station to dock bike."}
+    expect { 21.times { subject.dock(Bike.new) } }.to raise_error{"Docking station at default capacity is full."}
   end
-
 
   it "should release a working bike" do
     bike = Bike.new
@@ -40,7 +39,7 @@ describe DockingStation do
   it "should, if a capacity is specified, have the specified capacity" do
     station = DockingStation.new(10)
     10.times { station.dock(Bike.new) }
-    expect { station.dock(Bike.new) }.to raise_error("Docking station is full.")
+    expect { station.dock(Bike.new) }.to raise_error{"Docking station at specified capacity is full."}
   end
 
 end
